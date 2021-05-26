17196bee0a4bc042e70b315b35f9282eb54ca74e
Andy checo <savedcity@Andys-MacBook-Air.local>
2021-05-25 21:06:56 -0500

    auth

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Products from "./components/Products";
+import Users from "./components/Users";
+import fire from "./config/fire";
 
 import axios from "axios";
 
 class App extends React.Component {
   state = {
     products: [],
+    users: {},
+  };
+
+  authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
   };
 
   getProducts = () => {
@@ -20,11 +33,14 @@ class App extends React.Component {
 
   componentDidMount = () => {
     this.getProducts();
+    this.authListener();
   };
 
   render = () => {
     return (
       <div>
+        {this.state.user ? <h4> You are logged in! </h4> : <Users />}
+
         <h1>Spamazon's black market (keep secret)</h1>
         {this.state.products.map((item) => {
           return (
---
+++ storefront/src/config/fire.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyARb8CCUmpCK2reoQWJzZWH6ly1j3adEfk",
+  authDomain: "sp-a24ba.firebaseapp.com",
+  projectId: "sp-a24ba",
+  storageBucket: "sp-a24ba.appspot.com",
+  messagingSenderId: "914422664124",
+  appId: "1:914422664124:web:392360ae0f6e3e617bb9b9",
+  measurementId: "G-ERM300KQ8S",
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
