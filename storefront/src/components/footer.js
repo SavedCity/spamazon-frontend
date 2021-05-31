9b01a5bdc8872302563b6d11c52cc3dc1e322566
Anthourn <80663362+Anthourn@users.noreply.github.com>
2021-05-31 16:23:01 -0400

    footer

---
+++ storefront/src/components/footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Footer extends Component {
+render () {
+    return (
+        <div className = 'footer'>
+        <p>Spamazon designed in collaboration by Andy Checo, Jessie Moryn, and Troy Redway</p>
+        </div>
+    )
+}
+}
